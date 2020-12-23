72642100e809e873680dd19441a148bf5956e180
vyasishanatc194 <vyasishanatc194@gmail.com>
2020-12-23 14:31:14 +0530

    fist commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django-Blog-App
+A blog application using Django
---
+++ blog/views.py
@@ -0,0 +1,37 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+
+
+
+
